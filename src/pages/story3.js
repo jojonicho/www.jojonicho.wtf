e2ce3e7142eaf5dd59560d5ffa0c424f8f4d1f11
Jonathan Nicholas <jojonicho181@gmail.com>
2020-02-09 16:46:45 +0700

    story3

---
+++ src/pages/story3.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import "./style.scss"
+import SEO from "../components/seo"
+import Slide from 'react-reveal/Slide';
+import SVG from "../components/SVG"
+
+const Navbar = () => {
+    const [active, setActive] = useState('false');
+    return (
+        <nav className="navbar is-danger" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <div className="home">
+                    <Link to="/">
+                        <SVG name="Pokeball" />
+                    </Link>
+                </div>
+                <Link role="button" onClick={() => setActive(!active)} className={`navbar-burger ${(active === false) ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </Link>
+            </div>
+            <Slide top distance='15px' duration={600}>
+                <div className={`navbar-menu ${(active === false) ? 'is-active' : ''}`}>
+                    <div className="navbar-end is-one-quarter">
+                        <Link to="/blog" className="navbar-item is-narrow">
+                            <p>Blog</p>
+                        </Link>
+                    </div>
+                </div>
+            </Slide>
+        </nav>
+    );
+}
+
+const Story3 = () => {
+
+    return (
+        <>
+            <Navbar />
+            <div className="section container">
+                <SEO title="Jonathan Nicholas" />
+                <SEO title="jojonicho" />
+                <div className="columns is-7 is-mobile is-vcentered">
+                    <div className="column is-one-fourth me">
+                        {/* <SVG name="Square" className="square"/> */}
+                    <img src={"/jonathan.jpg"} alt="jonathan" />
+                    </div>
+                        <div className="column is-two-thirds">
+                            {/* <p className="subtitle"> hello, my name is </p> */}
+                            <p className="myname">Hello, my name is </p> <p className="has-text-danger myname">Jonathan Nicholas</p>
+                        </div>
+                    </div>
+            </div>
+            <div>
+                <SVG name='blob' />
+            </div>
+            <div className="section container">
+                <p className="title has-text-centered">My Projects</p>
+                <div className="container proj">
+                <img src={"/myproj.png"} alt="myproj" />
+                </div>
+            </div>
+            <div className='section'>
+            <p className="title has-text-centered">Interested?<br/>Check out my <p className="has-text-centered has-text-danger">CV</p></p>
+            </div>
+        </>
+    );
+}
+
+export default Story3
